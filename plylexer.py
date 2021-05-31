bc383b2601cf01fe8eb18e3785a6669375de4633
FgRogelio <rfelix@owd.mx>
2021-05-31 14:29:01 -0600

    Renaming and refactorization

--- plylexer.py
+++ plylexer.py
@@ -38,9 +38,9 @@ t_GTREQTHAN = r'>='
 
 t_LSSEQTHAN = r'<='
 
-t_NUMI = r'\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
+t_NUMI = r'-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
 
-t_NUMF = r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
+t_NUMF = r'-?((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
 
 t_STR = r'\"([^\\\n]|(\\.))*?\"'
 
@@ -59,4 +59,4 @@ def t_error(t):
 	print("Illegal character %s" % repr(t.value[0]))
 	t.lexer.skip(1)
 
-lex.lex()
+lx = lex.lex()
