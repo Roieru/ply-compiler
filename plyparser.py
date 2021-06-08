34410e8c8ed786dab747fb5cc86a0936177a2e39
FgRogelio <rfelix@owd.mx>
2021-06-07 18:11:38 -0600

    Grammar correction

--- plyparser.py
+++ plyparser.py
@@ -1,406 +1,212 @@
-import ply.yacc as yacc
-from plylexer import tokens, literals
-import plylexer
+from plyparser import Node, root, printChildren
 import sys
+import re
+import copy
 
-class Node:
-    def __init__(self,type,children=None):
-        self.type = type
-        if children:
-            self.children = children
-        else:
-            self.children = [ ]
+class VariablePLY:
+    def __init__(self,value,typ):
+        self.typ = typ
+        self.value = value
 
-def boolToTree(input):
+variables = { }
 
-    stack = []
-    output = []
-    while(len(input) > 0):
-        e = input.pop(0)
-        if(e == "("):
-            stack.append(e)
-        elif(e == ")"):
-            while(len(stack) > 0 and stack[len(stack)-1] != "("):
-                output.append(stack.pop())
-            if(stack[len(stack)-1] == "("):
-                stack.pop()
-            else:
-                sys.exit("Error 2")
-        elif(e in ["and", "or"]):
-            if(len(stack) > 0 and stack[len(stack)-1] in ["and", "or"]):
-                output.append(stack.pop())
-            stack.append(e)
-        else:
-            output.append(e)
-    while(len(stack) > 0):
-        output.append(stack.pop())
-
-    stack = []
-    input = output
-
-    while(len(input) > 0):
-        e = input.pop(0)
-        if(isinstance(e, Node)):
-            stack.append(e)
-        elif(e == '('):
-            sys.exit("Error 2")
-        elif(e in ["and", "or"]):
-            if(len(stack) < 2):
-                sys.exit("Error 3")
-            else:
-                a2 = stack.pop()
-                if(not(isinstance(a2, Node))):
-                    a2 = Node(a2)
-                a1 = stack.pop()
-                if(not(isinstance(a1, Node))):
-                    a1 = Node(a1)
-                stack.append(Node(e,children=[a1, a2]))
-        else:
-            stack.append(e)
-    if(len(stack) != 1):
-        sys.exit("Error 3")
+def getVarType(node, varName):
+    if node in variables.keys() and varName in (o.value for o in variables[node]):
+        return [x for x in variables[node] if x.value == varName][0].typ
+    if node.type == "else" or node.type == "elif":
+        currentNode = node.parent
+        while((currentNode.type == "if" or currentNode.type == "elif") and currentNode.parent):
+            currentNode = currentNode.parent
+        return getVarType(currentNode, varName)
+    if node.parent:
+        return getVarType(node.parent, varName)
     else:
-        e = stack.pop()
-        if(not(isinstance(e, Node))):
-            e = Node(e)
-        return e
+        return None
 
-def hasGreaterPrecedence(a, b):
-    if(a in "^"):
+def findScopeNode(node):
+    if(node.type in ["block", "for", "dowh", "if", "elif", "else", "while"]):
+        return node
+    if(node.parent):
+        return findScopeNode(node.parent)
+    return Node('Self')
+
+def isVarInTree(node, varName):
+    if node.children:
+        for child in node.children:
+            isVarInTree(child, varName)
+    elif node.type == varName:
+        sys.exit("[ ! ] Invalid assignment.")
+
+def printVariables(r):
+    if r.type == "declaration":
+        print(r.children[0].type + " declared as " + r.children[1].type + " inside a " + findScopeNode(r).type)
+        scopeNode = findScopeNode(r)
+        if scopeNode in variables.keys():
+            variables[scopeNode].append(VariablePLY(r.children[0].type, r.children[1].type))
+        else:
+            variables[scopeNode] = [VariablePLY(r.children[0].type, r.children[1].type)]
+    if r.children:
+        for child in r.children:
+            printVariables(child)
+
+def isWithinScope(node, varName):
+    if node in variables.keys() and varName in (o.value for o in variables[node]):
         return True
-    elif(a in "*/" and b in "*/+-"):
-        return True
-    elif(a in "+-" and b in "+-"):
-        return True
+    if node.type == "else" or node.type == "elif":
+        currentNode = node.parent
+        while((currentNode.type == "if" or currentNode.type == "elif") and currentNode.parent):
+            currentNode = currentNode.parent
+        return isWithinScope(currentNode, varName)
+    if node.parent:
+        return isWithinScope(node.parent, varName)
     else:
         return False
 
-def treeFromInfix(input):
-
-    #Infix to Postfix
-
-    stack = []
-    output = []
-    while(len(input) > 0):
-        e = input.pop(0)
-        if(e == "("):
-            stack.append(e)
-        elif(e == ")"):
-            while(len(stack) > 0 and stack[len(stack)-1] != "("):
-                output.append(stack.pop())
-            if(stack[len(stack)-1] == "("):
-                stack.pop()
-            else:
-                sys.exit("Error 2")
-        elif(e in "+-/*^"):
-            while(len(stack) > 0 and hasGreaterPrecedence(stack[len(stack)-1],e)):
-                output.append(stack.pop())
-            stack.append(e)
-        else:
-            output.append(e)
-    while(len(stack) > 0):
-        output.append(stack.pop())
-
-    #Postfix to Tree
-
-    stack = []
-    input = output
-
-    while(len(input) > 0):
-        e = input.pop(0)
-        if(isinstance(e, Node)):
-            stack.append(e)
-        elif(e == '('):
-            sys.exit("Error 2")
-        elif(e in "+-*/^"):
-            if(len(stack) < 2):
-                sys.exit("Error 3")
-            else:
-                a2 = stack.pop()
-                if(not(isinstance(a2, Node))):
-                    a2 = Node(a2)
-                a1 = stack.pop()
-                if(not(isinstance(a1, Node))):
-                    a1 = Node(a1)
-                stack.append(Node(e,children=[a1, a2]))
-        else:
-            stack.append(e)
-    if(len(stack) != 1):
-        sys.exit("Error 3")
-    else:
-        e = stack.pop()
-        if(not(isinstance(e, Node))):
-            e = Node(e)
-        return e
-
-def p_block(p):
-    '''
-    block : stmt block
-        | stmt
-    '''
-    if(len(p) > 2):
-        p[0] = Node('block', children=[p[1], p[2]])
-    else:
-        p[0] = p[1]
-
-def p_stmt(p):
-    '''
-    stmt : simpstmt ';'
-        | flowctrl
-    '''
-    p[0] = p[1]
-
-def p_simpstmt_assdec(p):
-    '''
-    simpstmt : type ID '=' expr
-    '''
-    d = Node('declaration', [Node(p[2]), p[1]])
-    p[0] = Node('assignment', [d, p[4]])
-
-def p_simpstmt_dec(p):
-    '''
-    simpstmt : type ID
-    '''
-    p[0] = Node('declaration', [Node(p[2]), p[1]])
-
-def p_simpstmt_ass(p):
-    '''
-    simpstmt : ID '=' expr
-    '''
-    p[0] = Node('assignment', [Node(p[1]), p[3]])
-
-def p_type(p):
-    '''
-    type : BOOLEAN
-        | FLOAT
-        | INT
-        | STRING
-    '''
-    p[0] = Node(p[1])
-
-def p_expr_num(p):
-    '''
-    expr : numexpr
-    '''
-    p[0] = treeFromInfix(p[1])
-
-def p_expr_str(p):
-    '''
-    expr : strexpr
-    '''
-    p[0] = p[1]
-
-def p_expr_bool(p):
-    '''
-    expr : boolexpr
-    '''
-    p[0] = p[1]
-
-def p_flowctrl_for(p):
-    '''
-    flowctrl : FOR '(' simpstmt ';' boolexpr ';' simpstmt ')' '{' block '}'
-    '''
-    p[0] = Node('for', [p[3], p[5], p[7], p[10]])
-
-def p_flowctrl_dowhile(p):
-    '''
-    flowctrl : DO '{' block '}' WHILE '(' boolexpr ')' ';'
-    '''
-    p[0] = Node('dowh', [p[3], p[7]])
-
-def p_flowctrl_while(p):
-    '''
-    flowctrl : WHILE '(' boolexpr ')' '{' block '}'
-    '''
-    p[0] = Node('while', [p[3], p[6]])
-
-def p_flowctrl_if(p):
-    '''
-    flowctrl : IF '(' boolexpr ')' '{' block '}' elif else
-    '''
-    if(len(p) > 2):
-        ch = [p[3], p[6]]
-        if(p[8]):
-            ch.append(p[8])
-        if(p[9]):
-            ch.append(p[9])
-        p[0] = Node('if', children=ch)
-
-def p_elif(p):
-    '''
-    elif : ELIF '(' boolexpr ')' '{' block '}' elif
-        | empty
-    '''
-    if(len(p) > 2):
-        ch = [p[3], p[6]]
-        if(p[8]):
-            ch.append(p[8])
-        p[0] = Node('elif', children=ch)
-
-def p_else(p):
-    '''
-    else : ELSE '{' block '}'
-        | empty
-    '''
-    if(len(p) > 2):
-        p[0] = p[3]
-
-def p_numexpr_num(p):
-    '''
-    numexpr : num
-    '''
-    p[0] = [p[1]]
-
-def p_numexpr_arit(p):
-    '''
-    numexpr : numexpr arit numexpr
-    '''
-    p[0] = []
-    for i in p[1]:
-        p[0].append(i)
-    p[0].append(p[2])
-    for i in p[3]:
-        p[0].append(i)
-
-def p_numexpr_par(p):
-    '''
-    numexpr : '(' numexpr ')'
-    '''
-    p[0] = []
-    p[0].append(p[1])
-    for i in p[2]:
-        p[0].append(i)
-    p[0].append(p[3])
-
-def p_num(p):
-    '''
-    num : NUMI
-        | NUMF
-        | ID
-    '''
-    p[0] = p[1]
-
-def p_arit(p):
-    '''
-    arit : '+'
-        | '-'
-        | '*'
-        | '/'
-        | '^'
-    '''
-    p[0] = p[1]
-
-def p_strexpr_one(p):
-    '''
-    strexpr : concat
-    '''
-    p[0] = p[1]
-
-def p_strexpr_concat(p):
-    '''
-    strexpr : concat '+' concat
-    '''
-    p[0] = Node('concat', [p[1], p[3]])
-
-def p_strexpr_par(p):
-    '''
-    strexpr : '(' strexpr ')'
-    '''
-    p[0] = Node(p[2])
-
-def p_concat_one(p):
-    '''
-    concat : STR
-        | ID
-    '''
-    p[0] = Node(p[1])
-
-def p_concat_par(p):
-    '''
-    concat : STRING '(' numexpr ')'
-    '''
-    p[0] = treeFromInfix(p[3])
-
-def p_boolexpr_bin(p):
-    '''
-    boolexpr : boolexpr AND boolexpr
-        | boolexpr OR boolexpr
-        | boolexpr EQUALS boolexpr
-        | boolexpr NOTEQUALS boolexpr
-    '''
-    p[0] = Node(p[2], [p[1], p[3]])
-
-def p_boolexpr_one(p):
-    '''
-    boolexpr : boolop
-    '''
-    p[0] = p[1]
-
-def p_boolexpr_par(p):
-    '''
-    boolexpr : '(' boolexpr ')'
-    '''
-    p[0] = p[2]
-
-def p_boolop(p):
-    '''
-    boolop : strcomp
-        | numcomp
-        | bool
-    '''
-    p[0] = p[1]
-
-def p_bool(p):
-    '''
-    bool : TRUE
-        | FALSE
-        | ID
-    '''
-    p[0] = Node(p[1])
-
-def p_strcomp(p):
-    '''
-    strcomp : strexpr NOTEQUALS strexpr
-        | strexpr EQUALS strexpr
-    '''
-    p[0] = Node(p[2], [p[1], p[3]])
-
-def p_numcomp(p):
-    '''
-    numcomp : numexpr comp numexpr
-    '''
-    p[0] = Node(p[2], [treeFromInfix(p[1]), treeFromInfix(p[3])])
-        
-def p_comp(p):
-    '''
-    comp : EQUALS
-        | NOTEQUALS
-        | GTREQTHAN
-        | LSSEQTHAN
-        | '<'
-        | '>'
-    '''
-    p[0] = p[1]
-
-def p_empty(p):
-    'empty :'
-    pass
-
-def p_error(p):
-    pass
-
-parserer = yacc.yacc()
-
-root = parserer.parse(lexer=plylexer.lx, input=open("input.txt").read())
-
-def printChildren(node):
-    print(node.type)
+def treeNumTypeCheck(node):
     if node.children:
-        for i in node.children:
-            printChildren(i)
+        if not node.type in ["+", "-", "/", "*", "^"]:
+            sys.exit("[ ! ] Invalid number assignment.")
+        for child in node.children:
+            treeNumTypeCheck(child)
+        if node.children[0].ptype == node.children[1].ptype:
+            node.ptype = node.children[0].ptype
+        else:
+            for i in range(len(node.children)):
+                if node.children[i].ptype == "int":
+                    parseNode = Node('int2float', ptype="float")
+                    node.children[i].parent = parseNode
+                    parseNode.children = [node.children[i]]
+                    node.children[i] = parseNode
+            node.ptype = "float"
 
-#printChildren(boolToTree(['true', 'and', '(','false', 'or', 'false', ')']))
+    else:
+        if(re.fullmatch(r'-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?', node.type)):
+            node.ptype = "int"
+        elif(re.fullmatch(r'-?((\d+)(\.\d+)(e(\+|-)?(\d+))?|(\d+)e(\+|-)?(\d+))([lL]|[fF])?', node.type)):
+            node.ptype = "float"
+        else:
+            if(not isWithinScope(node, node.type)):
+                sys.exit("[ ! ] Variable " + node.type + " has not been declared within scope.")
+            varType = getVarType(node, node.type)
+            if varType == "string" or varType == "boolean":
+                sys.exit("[ ! ] Can't convert " + varType + " to number.")
+            node.ptype = varType
+    #print(node.type + " is " + node.ptype)
 
-#root = treeFromInfix(['4', '-', '5'])
+def treeStrTypeCheck(node):
+    if node.type == "num2string":
+        treeNumTypeCheck(node.children[0])
+        node.type = node.children[0].ptype + "2string"
+        node.ptype = "string"
+    elif not node.children:
+        if not re.fullmatch(r'\"([^\\\n]|(\\.))*?\"', node.type):
+            if not re.fullmatch(r'[A-Za-z_][\w_]*', node.type):
+                sys.exit("[ ! ] Can't convert "+ node.type + " to string.")
+            if not isWithinScope(node, node.type):
+                sys.exit("[ ! ] Variable " + node.type + " has not been declared within scope.")
+            varType = getVarType(node, node.type)
+            if varType != "string":
+                sys.exit("[ ! ] Can't convert " + varType + " to string.")
+        node.ptype = "string"
+    else:
+        if node.type != "concat":
+            sys.exit("[ ! ] Invalid string assignment.")
+        for child in node.children:
+            treeStrTypeCheck(child)
 
+def treeBoolTypeCheck(node):
+    if not node.children:
+        if node.type != "true" and node.type != "false":
+            if not isWithinScope(node, node.type):
+                sys.exit("[ ! ] Variable " + node.type + " has not been declared within scope.")
+            varType = getVarType(node, node.type)
+            if varType != "boolean":
+                sys.exit("[ ! ] Can't convert " + varType + " to boolean.")
+    elif node.type in ["==", "!="]:
+        if(node.children[0].type in ["+", "-", "/", "*", "^"] or re.match(r'-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?', node.children[0].type)):
+            treeNumTypeCheck(node.children[0])
+            treeNumTypeCheck(node.children[1])
+        elif(node.children[0].type in ["concat", "num2string"] or re.fullmatch(r'\"([^\\\n]|(\\.))*?\"', node.children[0].type)):
+            treeStrTypeCheck(node.children[0])
+            treeStrTypeCheck(node.children[1])
+        elif(node.children[0].type in ["==", "!=", "<", ">", ">=", "<=", "and", "or", "true", "false"]):
+            treeBoolTypeCheck(node.children[0])
+            treeBoolTypeCheck(node.children[1])
+        else:
+            child0Type = getVarType(node, node.children[0].type)
+            if child0Type == "float" or child0Type == "int":
+                treeNumTypeCheck(node.children[1])
+            elif child0Type == "string":
+                treeStrTypeCheck(node.children[1])
+            elif child0Type == "boolean":
+                treeBoolTypeCheck(node.children[1])
+            else:
+                sys.exit("[ ! ] Variable " + node.children[0].type + " has not been declared within scope.")
+    elif node.type in [">", "<", ">=", "<="]:
+        treeNumTypeCheck(node.children[0])
+        treeNumTypeCheck(node.children[1])
+    elif node.type in ["and", "or"]:
+            treeBoolTypeCheck(node.children[0])
+            treeBoolTypeCheck(node.children[1])
+
+    node.ptype = "boolean"
+
+def setVariables(r):
+    if(r.type == "declaration"):
+        if isWithinScope(r, r.children[0].type):
+            sys.exit("[ ! ] Variable " + r.children[0].type + " has already been declared within scope.")
+        #print(r.children[0].type + " declared as " + r.children[1].type + " inside a " + findScopeNode(r).type)
+        scopeNode = findScopeNode(r)
+        if scopeNode in variables.keys():
+            variables[scopeNode].append(VariablePLY(r.children[0].type, r.children[1].type))
+        else:
+            variables[scopeNode] = [VariablePLY(r.children[0].type, r.children[1].type)]
+    if r.children:
+        for child in r.children:
+            setVariables(child)
+
+def semanticAnalysis(r):
+
+    checkChildren = True
+
+    if(r.type == "assignment"):
+        correctType = ""
+        if r.children[0].type == "declaration":
+            correctType = r.children[0].children[1].type
+            varName = r.children[0].children[0].type
+            isVarInTree(r.children[1], varName)
+        elif (not isWithinScope(r, r.children[0].type)):
+            sys.exit("[ ! ] Variable " + r.children[0].type + " has not been declared within scope.")
+        else:
+            correctType = getVarType(r, r.children[0].type)
+        if correctType == "int" or correctType == "float":
+            treeNumTypeCheck(r.children[1])
+            if correctType == "float" and r.children[1].ptype == "int":
+                parseNode = Node('int2float', ptype="float")
+                r.children[1].parent = parseNode
+                parseNode.children = [r.children[1]]
+                r.children[1] = parseNode
+            elif r.children[1].ptype != correctType:
+                sys.exit("[ ! ] Can't convert " + r.children[1].ptype + " to " + correctType + ".")
+        elif correctType == "string":
+            treeStrTypeCheck(r.children[1])
+        elif correctType == "boolean":
+            treeBoolTypeCheck(r.children[1])
+        checkChildren = False
+    elif(r.type in ["+", "-", "/", "*", "^"] or re.match(r'-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?', r.type)):
+        treeNumTypeCheck(r)
+        checkChildren = False
+    elif(r.type in ["concat", "num2string"] or re.fullmatch(r'\"([^\\\n]|(\\.))*?\"', r.type)):
+        treeStrTypeCheck(r)
+        checkChildren = False
+    elif(r.type in ["==", "!=", "<", ">", ">=", "<=", "and", "or", "true", "false"]):
+        treeBoolTypeCheck(r)
+        checkChildren = False
+    if r.children and checkChildren:
+        for child in r.children:
+            semanticAnalysis(child)
+
+#printVariables(root)
+setVariables(root)
+semanticAnalysis(root)
 printChildren(root)
